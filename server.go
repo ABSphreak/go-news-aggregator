eb8f6fe592f7956bf3c66413c3c975630929071c
ABSphreak <absphreak@outlook.com>
2020-12-29 21:15:37 +0530

    ➕ Added Map & Error detection

--- server.go
+++ server.go
@@ -3,27 +3,55 @@ package main
 import ("fmt"
 "net/http"
 "io/ioutil"
-"encoding/xml")
+"encoding/xml"
+"strings")
 
 type SitemapIndex struct {
-	Locations []Location `xml:"sitemap"`
+	Locations []string `xml:"sitemap>loc"`
 }
 
-type Location struct {
-	Loc string `xml:"loc"`
+type News struct {
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
 }
 
-func (l Location) String() string {
-	return fmt.Sprintf(l.Loc)
+type NewsMap struct {
+	Keyword string
+	Location string
 }
 
 func main() {
+	var s SitemapIndex
+	var n News
+	newsMap := make(map[string]NewsMap)
+
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
-
-	var s SitemapIndex
 	xml.Unmarshal(bytes, &s)
 
-	fmt.Println(s.Locations)
+	for _, Location := range s.Locations {
+		Location = strings.TrimSpace(Location)
+		Location = strings.Replace(Location, "\n", "", -1)
+		resp, err := http.Get(Location)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println("Successfully read → ", Location)
+		}
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		xml.Unmarshal(bytes, &n)
+
+		for idx := range n.Keywords {
+			newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+	}
+
+	for idx, data := range newsMap {
+		fmt.Println("\n\n\n", idx)
+		fmt.Println("\n", data.Keyword)
+		fmt.Println("\n", data.Location)
+	}
 }
